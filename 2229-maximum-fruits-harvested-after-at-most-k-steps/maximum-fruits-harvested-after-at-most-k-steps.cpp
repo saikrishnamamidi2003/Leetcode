32640f78077ba5157b179c017d6001e3a76adbfe
saikrishnamamidi2003 <139036109+saikrishnamamidi2003@users.noreply.github.com>
2025-08-03 13:36:00 +0530

    Time: 4 ms (73.62%) | Memory: 124.4 MB (96.17%) - LeetSync

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
-        int n = fruits.size();
-        vector<int> sum(n + 1);
-        vector<int> indices(n);
-        for (int i = 0; i < n; i++) {
-            sum[i + 1] = sum[i] + fruits[i][1];
-            indices[i] = fruits[i][0];
+        int left = 0, sum = 0, maxFruits = 0;
+        for (int right = 0; right < fruits.size(); ++right) {
+            sum += fruits[right][1];
+            while (left <= right && minSteps(fruits[left][0], fruits[right][0], startPos) > k) {
+                sum -= fruits[left][1];
+                left++;
+            }
+            maxFruits = max(maxFruits, sum);
         }
-        int ans = 0;
-        for (int x = 0; x <= k / 2; x++) {
-            /* move left x steps, then move right k - x steps. */
-            int y = k - 2 * x;
-            int left = startPos - x;
-            int right = startPos + y;
-            int start = lower_bound(indices.begin(), indices.end(), left) -
-                        indices.begin();
-            int end = upper_bound(indices.begin(), indices.end(), right) -
-                      indices.begin();
-            ans = max(ans, sum[end] - sum[start]);
-            /* move right x steps, then move left k - x steps. */
-            y = k - 2 * x;
-            left = startPos - y;
-            right = startPos + x;
-            start = lower_bound(indices.begin(), indices.end(), left) -
-                    indices.begin();
-            end = upper_bound(indices.begin(), indices.end(), right) -
-                  indices.begin();
-            ans = max(ans, sum[end] - sum[start]);
-        }
-        return ans;
+        return maxFruits;
+    }
+
+    int minSteps(int left, int right, int start) {
+        return min(abs(start - left) + (right - left),
+                   abs(start - right) + (right - left));
     }
 };
