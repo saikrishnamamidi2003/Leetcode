bf5d9c34431b9da3316afc105ca6122f794524ac
saikrishnamamidi2003 <139036109+saikrishnamamidi2003@users.noreply.github.com>
2026-01-20 21:24:59 +0530

    Time: 1420 ms (23.75%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -8,20 +8,20 @@
 -- We want the count per customer, not for the whole table
 
 select customer_id, count(visit_id) as count_no_trans
--- from Visits
--- where (
---     visit_id not in (
---        select visit_id from Transactions 
---     )
--- )
--- Group By customer_id 
--- select customer_id, count(visit_id) as count_no_trans
 from Visits
 where (
     visit_id not in (
-       select visit_id from Transactions
+       select visit_id from Transactions 
     )
-
 )
-Group By customer_id
+Group By customer_id 
+-- select customer_id, count(visit_id) as count_no_trans
+-- from Visits
+-- where (
+    -- visit_id not in (
+    --    select visit_id from Transactions
+    -- )
+
+-- )
+-- Group By customer_id
 #we kept customer id as group because in the first colum there is custumer id -> so dhaniki thaggattu pakkana group laa raavalii , dhaniki match kaadaniki.
