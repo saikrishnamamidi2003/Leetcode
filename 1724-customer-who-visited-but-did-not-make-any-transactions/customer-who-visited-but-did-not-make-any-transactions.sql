d8e33f0f909f29fa9cdd5049120fac89dadcadce
saikrishnamamidi2003 <139036109+saikrishnamamidi2003@users.noreply.github.com>
2026-01-20 20:21:44 +0530

    Time: 1255 ms (57.68%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,9 +1,18 @@
 # Write your MySQL query statement below
+-- 🔑 WHY GROUP BY IS NEEDED HERE
+
+-- Because:
+
+-- COUNT(visit_id) is an aggregate function
+
+-- We want the count per customer, not for the whole table
+
 select customer_id, count(visit_id) as count_no_trans
 from Visits
-where 
+where (
     visit_id not in (
-        select visit_id from Transactions
+       select visit_id from Transactions
     )
-group by customer_id 
--- if you are using anything like count or any max or sum function then use the group function to make that total count to be group one...
+
+)
+Group By customer_id
