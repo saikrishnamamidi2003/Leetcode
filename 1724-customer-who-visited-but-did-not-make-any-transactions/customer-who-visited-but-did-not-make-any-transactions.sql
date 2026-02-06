25863df17e15e153894c41318bf071248093af91
saikrishnamamidi2003 <139036109+saikrishnamamidi2003@users.noreply.github.com>
2026-02-06 16:25:37 +0530

    Time: 1134 ms (95.18%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,27 +1,9 @@
 # Write your MySQL query statement below
--- 🔑 WHY GROUP BY IS NEEDED HERE
-
--- Because:
-
--- COUNT(visit_id) is an aggregate function
-
--- We want the count per customer, not for the whole table
-
 select customer_id, count(visit_id) as count_no_trans
-from Visits
+from Visits v
 where (
-    visit_id not in (
-       select visit_id from Transactions 
+    v.visit_id not in(
+        select visit_id from Transactions
     )
 )
- Group By customer_id 
--- select customer_id, count(visit_id) as count_no_trans
--- from Visits
--- where (
-    -- visit_id not in (
-    --    select visit_id from Transactions
-    -- )
-
--- )
--- Group By customer_id
-#we kept customer id as group because in the first colum there is custumer id -> so dhaniki thaggattu pakkana group laa raavalii , dhaniki match kaadaniki.
+Group By customer_id
